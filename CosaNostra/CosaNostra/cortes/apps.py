2ae6db57f0561a9860fe817d6d41047e7c584e58
redcliver <igor-peres@hotmail.com>
2017-07-03 14:43:03 -0400

    teste

---
+++ CosaNostra/CosaNostra/cortes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CortesConfig(AppConfig):
+    name = 'cortes'
---
+++ CosaNostra/CosaNostra/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\IGOR\Source\Repos\CosaNostra1\CosaNostra\CosaNostra\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
